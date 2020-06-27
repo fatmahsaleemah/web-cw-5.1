b79bafa0ed1ae3d7d3984c59e37f987bcc10550a
fatmahsaleemah <66628606+fatmahsaleemah@users.noreply.github.com>
2020-06-27 20:27:05 +0300

    تم الحل

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,36 +1,49 @@
 // السؤال الأول
 // 1
-let name = "fatmah";
+const name = "fatmah";
 // 2
-const b = 18;
+let age = 18;
 // 3 and 4
-print(name+" "+b);
+
+Console.log(name+" "+age)
 // 5 and 6
-Console.log(name+" "+b)
+Console.log("انا"+" "+"فاطمه")
+// 5 and 6
 let  counter = 18;
 counter = counter * 5
+Console.log("counter")
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let f = ["الرسم، البرمجه، ١٨"]
 
 // 2
-
+f.push("18")
 // 3
-
+f.pop()
 // 4
 
+let u = {
+    name: "web",
+    k: "javascrept" ,
+ }
 // 5
+let h = ["نانسي", "موضي، ", "حسين","رهف"]
 
 // 6
-
+Console.log(u.name)
 // 7
-
+Console.log(f[0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
 
+function f(p1,p2){
+    return p1*p2
+}
+document.getElementById("demo").innerHTML = myFunction(انا, مبرمجه);
 // 2
 
 // console results => console.png
